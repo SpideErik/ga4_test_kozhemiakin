874a67cceb8ff0ccd9b963bd26684206c3806dbe
SpideErik <erikozh@gmail.com>
2026-09-17 17:22:16 +0300

    feat: add button with event cta_click

--- js/script.js
+++ js/script.js
@@ -16,3 +16,13 @@ if (form) {
   });
 }
 
+const programCta = document.querySelector('#program-cta');
+if (programCta) {
+  programCta.addEventListener('click', () => {
+    document.querySelector('#program-preview').hidden = false;
+    gtag('event', 'cta_click', {
+      button_name: 'program',
+      page_section: 'hero'
+    });
+  });
+}
